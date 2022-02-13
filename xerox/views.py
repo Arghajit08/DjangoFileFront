56feeaee602eaecd30867eaa4dd99009cb25b41f
Arghajit08 <89945847+Arghajit08@users.noreply.github.com>
2022-02-13 15:58:11 +0530

    Update views.py

--- xerox/views.py
+++ xerox/views.py
@@ -1,18 +1,29 @@
 from django.http import HttpResponse
 from django.shortcuts import render,redirect
 def home(request):
+    from django.shortcuts import render,redirect
     return render(request,"index.html")
 def result(request):
+    from django.shortcuts import render,redirect
     name=request.GET['name']
     address=request.GET['P']
     number=request.GET['G']
     document=request.GET['category']
     print(document)
     from twilio.rest import Client
+    import random
     
     sid='ACad20495dd3bf324541f3c9a60657ddf9'
-    authToken='4a8b0175ded1c5f4c01a098919264e0f'
+    authToken='11cfb1773aeb5e15fb8d490c9b694bd3'
+
     client=Client(sid,authToken)
+    str_number='+91'+str(number)
+
+    
+    sid1='ACdc82b5afee95e7d01d983bfa471b869a'
+    authToken1='1ef64a3025557673b50064bc1b1009e4'
+    
+    client1=Client(sid1,authToken1)
 
     from_whatsapp_number='whatsapp:+14155238886'
     to_whatsapp_number='whatsapp:+919863103113'
@@ -32,5 +43,21 @@ def result(request):
     message=client.messages.create(body=document,
                                    from_=from_whatsapp_number,
                                    to=to_whatsapp_number)
+    import requests
+    url='https://www.fast2sms.com/dev/bulkV2'
+    n=random.randint(1000,9999)
+    message=str(n)
+    numbers=str(number)
+    payload= f'sender_id=TXTIND&message={message}&language=english&route=p&numbers={numbers}'
+    headers= {'authorization':'HGZXp9BAmOQjh8LKIaf3F75UuSrkYtWEd0neslioVTzJgPxNR4H0Z3RoUcaJxEsFCX8nWQgPmItKVpGd','Content-Type':'application/x-www-form-urlencoded'}
+    response=requests.request('POST',url,data=payload,headers=headers)
+    print(response.text)
+    return render(request,"index1.html",{'otp':n})
     
-    return redirect("https://xeroxfin.herokuapp.com/")
+def otp_verification(request):
+    actual=request.GET['otp1']
+    new=request.GET['abc']
+    if actual==new:
+        return redirect("https://xeroxfin.herokuapp.com/")
+    else:
+        return render(request,"index2.html")
